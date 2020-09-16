2b7b8aa02d709e4a1f629f87cd70d56a59f4a0df
Mark Lysenko <mark.fm257@gmail.com>
2020-09-16 18:40:01 +0300

    add searchBar and realized clicked

--- TodoListApp/Controllers/TodoListTableViewController.swift
+++ TodoListApp/Controllers/TodoListTableViewController.swift
@@ -93,3 +93,24 @@ extension TodoListTableViewController {
         tableView.deselectRow(at: indexPath, animated: true)
     }
 }
+
+//MARK: - Search bar delegate
+extension TodoListTableViewController: UISearchBarDelegate {
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        let request: NSFetchRequest<Item> = Item.fetchRequest()
+        let predicate = NSPredicate(format: "title CONTAINS[cd] %@", searchBar.text!)
+        request.predicate = predicate
+        let sortDescriptor =  NSSortDescriptor(key: "title", ascending: true)
+        request.sortDescriptors = [sortDescriptor ]
+        
+        do {
+            itemArray = try contex.fetch(request)
+        } catch {
+            print("Error fetching data from contex, \(error)")
+        }
+        
+        DispatchQueue.main.async {
+            self.tableView.reloadData()
+        }
+    }
+}
